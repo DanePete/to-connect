edeccbc96fd5d2e8099c0d31756741dc6599cf7e
dane <dane.m.petersen@gmail.com>
2021-10-20 10:00:50 -0500

    added react navbar and got up and running with it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import React, { useState } from 'react';
 import Dashboard from './components/Dashboard/Dashboard';
+import SideBar from './components/Dashboard/SideBar/SideBar';
+import { IntlProvider } from 'react-intl';
+import Layout from './components/Dashboard/Layout/Layout';
+import Messages from './components/Dashboard/Messages/Messages';
+import 'react-pro-sidebar/dist/css/styles.css';
 
 function App() {
+  const [locale, setLocale] = useState('en');
   return (
-    <div className="App">
-      <header className="App-header">
-        <Dashboard />
-      </header>
-    </div>
+    <IntlProvider locale={locale} messages={Messages[locale]}>
+      <Layout setLocale={setLocale} />
+    </IntlProvider>
   );
 }
 
