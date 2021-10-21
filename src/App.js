37e4e88c16a78e1136a8b69decbac57ba377ff38
dane <dane.m.petersen@gmail.com>
2021-10-21 16:41:37 -0500

    added in initial user profile creation on user cognito creation

--- src/App.js
+++ src/App.js
@@ -1,39 +1,32 @@
 import './App.css';
 import { useState } from 'react';
 import { IntlProvider } from 'react-intl';
-import Amplify, { API, graphqlOperation } from 'aws-amplify'
+import Amplify, { API, graphqlOperation, Auth } from 'aws-amplify'
+import * as queries from './graphql/queries';
+// import { Auth, Storage } from 'aws-amplify';
 import Layout from './components/Layout/Layout';
+import { createUser } from './graphql/mutations';
 import Messages from './components/Messages/Messages';
 import 'react-pro-sidebar/dist/css/styles.css';
 import { Hub, Logger } from 'aws-amplify';
 
 const logger = new Logger('My-Logger');
 
-async function createUser() {
-  try {
-    // if (!formState.name || !formState.description) return
-    // const todo = { ...formState }
-    // setTodos([...todos, todo])
-    // setFormState(initialState)
-    // await API.graphql(graphqlOperation(createUser, {input: todo}))
-  } catch (err) {
-    console.log('error creating todo:', err)
-  }
-}
-
+/**
+ * Listener
+ * Listens for Auth Events.
+ * @param {*} data 
+ */
 const listener = (data) => {
+      console.log('data payload', data.payload.data.userSub);
     switch (data.payload.event) {
         case 'signIn':
             logger.info('user signed in');
-            console.log('user signed in');
+            checkIfUserHasProfile();
             break;
         case 'signUp':
             logger.info('user signed up');
-            console.log('user signed up');
-
-
-
-            
+            createUserFunc(data.payload.data.userSub);
             break;
         case 'signOut':
             logger.info('user signed out');
@@ -51,8 +44,41 @@ const listener = (data) => {
             logger.info('the Auth module is configured');
     }
 }
-
 Hub.listen('auth', listener);
+
+/**
+ * Create User Function
+ * This creates a record in dynamoDB used for Profile - different from cognito user account
+ * @param {user sub from aws} user 
+ */
+async function createUserFunc(user) {
+  try {
+    console.log('before successful user ccreation!');
+    await API.graphql(graphqlOperation(createUser, {input: {id: user, userId: user}}))
+    console.log('successful user ccreation!');
+  } catch (err) {
+    console.log('error creating user:', err)
+  }
+}
+
+/**
+ * Check if User Has Profile
+ * This logic needs refactoring but will address that down the road
+ * @param {user sub from aws} user 
+ */
+async function checkIfUserHasProfile(user) {
+  console.log('user id', user);
+  try {
+    console.log('before successful get user');
+    const userProfile = await API.graphql({ query: queries.getUser, variables: { id: '7c04b33b-4368-4691-8cc7-72a20c9427ab' }});
+    console.log('user profile', userProfile);
+  } catch (err) {
+    console.log('error getting user:', err)
+    createUserFunc(user);
+  }
+}
+
+
 
 function App() {
   const [locale, setLocale] = useState('en');
@@ -62,5 +88,6 @@ function App() {
     </IntlProvider>
   );
 }
+
 
 export default App;
